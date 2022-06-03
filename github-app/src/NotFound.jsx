1309dd7c14b7876ea5496167924cf9e35a268cc7
pablomarcel <pablomarcel@outlook.com>
2022-06-02 19:12:07 -0700

    added 404 image

--- github-app/src/NotFound.jsx
+++ github-app/src/NotFound.jsx
@@ -4,7 +4,7 @@ export default function NotFound(){
     return(
         <div>
             <h1>404</h1>
-            <img src="" alt="Not Found"/>
+            <img src="https://media2.giphy.com/media/3o84TYaWr47oRcCQfu/giphy.gif?cid=790b7611e9f23169bf51db4678cb463198b3fbfcae942c11&rid=giphy.gif&ct=g" alt="Not Found"/>
         </div>
     )
 }
